717d863906311bb194edc34686f7a100102a128b
Sergio Imai <104179443+sergioimai@users.noreply.github.com>
2022-05-13 17:16:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,48 @@
-//As "chaves" de criptografia que utilizaremos são:
-//`A letra "e" é convertida para "enter"`
-//`A letra "i" é convertida para "imes"`
-//`A letra "a" é convertida para "ai"`
-//`A letra "o" é convertida para "ober"`
-//`A letra "u" é convertida para "ufat"` 
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
 
-function criptografia() {
-    var textoDigitado = document.getElementById("cripto").value;
-    var textoResultado = document.getElementById("descripto");
-
-    if (textoDigitado.trim() === "") {
-        var popup = document.getElementById("popUp");
-        popup.classList.toggle("show");
-    } else {
-        
-        var textoCriptografado = textoDigitado.toLowerCase();
-        var resultado1 = textoCriptografado.replace(/e/g, "enter");
-        var resultado2 = resultado1.replace(/i/g, "imes");
-        var resultado3 = resultado2.replace(/a/g, "ai");
-        var resultado4 = resultado3.replace(/u/g, "ufat");
-        var resultado5 = resultado4.replace(/o/g, "ober");
-
-        textoResultado.value = resultado5;
-        } 
-    }
-
-function descriptografia() {
-    var textoDigitado = document.getElementById("cripto").value;
-    var textoResultado = document.getElementById("descripto");
-
-    if (textoDigitado.trim() === "") {
-        var popup = document.getElementById("popUp");
-        popup.classList.toggle("show");
-    } else {
-        var textoDescriptografado = textoDigitado;
-        var resultado1 = textoDescriptografado.replace(/enter/g, "e");
-        var resultado2 = resultado1.replace(/imes/g, "i");
-        var resultado3 = resultado2.replace(/ai/g, "a");
-        var resultado4 = resultado3.replace(/ufat/g, "u");
-        var resultado5 = resultado4.replace(/ober/g, "o");
-
-        textoResultado.value = resultado5;
-    }
-}    
-
-function copiar() {
-    var textoCopiar = document.getElementById("descripto");
-    textoCopiar.select();
-    navigator.clipboard.writeText(textoCopiar.value);
-
-    window.location.reload();
+function btnCripto() {
+    const textoEncriptado = encriptar(inputTexto.value);
+    mensagem.value = textoEncriptado;
+    mensagem.style.backgroundImage = "none";
 }
+
+function encriptar(stringEncriptada) {
+    let codificar = [["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+    stringEncriptada = stringEncriptada.normalize("NFD").replace(/[^a-zA-Z\s]/g, "");
+
+    for (let i = 0; i < codificar.length; i++) {
+        if (stringEncriptada.includes(codificar[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(codificar[i][0], codificar[i][1]);
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDescripto() {
+    const textoDescriptado = descriptar(inputTexto.value);
+    mensagem.value = textoDescriptado;
+    mensagem.style.backgroundImage = "none";
+}
+
+function descriptar(stringDescriptada) {
+    let descodificar = [["ai", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]];
+    
+    for (let i = 0; i < descodificar.length; i++) {
+        if(stringDescriptada.includes(descodificar[i][0])) {
+            stringDescriptada = stringDescriptada.replaceAll(descodificar[i][0], descodificar[i][1]);
+        }
+    }
+    return stringDescriptada;
+}
+
+function btnCopiar() {
+    const textoCopiado = document.querySelector(".mensagem");
+    textoCopiado.select();
+    navigator.clipboard.writeText(textoCopiado.value);
+    window.location.reload();
+} 
+
+
+
